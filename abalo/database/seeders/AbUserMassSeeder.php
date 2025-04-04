d74aea044ee9eb1907c3709246f531be6f9d4586
Bobby <Bobbyly04@gmail.com>
2025-04-04 16:11:00 +0200

    added README.md and some comments

--- abalo/database/seeders/AbUserMassSeeder.php
+++ abalo/database/seeders/AbUserMassSeeder.php
@@ -14,6 +14,7 @@ class AbUserMassSeeder extends Seeder
      */
     public function run(): void
     {
+        // Korrektur der Sequenz (id ist SERIAL Primary Key, also unter anderem auch unique)
         DB::select("SELECT setval('ab_user_id_seq', (SELECT MAX(id) FROM ab_user))");
         // Erzeugt 50 User-Datensätze
         AbUser::factory()->count(50)->create();
