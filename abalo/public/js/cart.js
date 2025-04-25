d79fdef26edfa5e1c9caf608d6c9ff2115067015
Bobby <Bobbyly04@gmail.com>
2025-04-25 19:19:16 +0200

    finished task10

---
+++ abalo/public/js/cart.js
@@ -0,0 +1,77 @@
+"use strict";
+
+let cart = [];
+
+document.addEventListener('DOMContentLoaded', function()  // DOMContentLoaded = wenn HTML geladen ist
+{
+    const allButtons = document.querySelectorAll('.add-to-cart');
+
+    allButtons.forEach(function(button)
+    {
+        button.addEventListener('click', function()  // wir verwenden anonyme Funktionen, weil sehr sexy und kürzer als function () {}...
+            {
+                // console.log("Button clicked");
+                const id = parseInt(button.dataset.id);
+                const name = button.dataset.name;
+                const price = parseFloat(button.dataset.price);
+
+                // console.log(id, name, price);
+
+                addToCart(id, name, price);
+            }
+        )
+    })
+})
+
+function addToCart (id, name, price)
+{
+
+    for (let i = 0; i < cart.length; i++) // return, falls item schon im warenkorb
+    {
+        if (cart[i].id === id)
+        {
+            return;
+        }
+    }
+
+    cart.push({id, name, price});
+
+    updateCart();
+    updatePrice();
+}
+
+function updateCart ()
+{
+    const ul = document.getElementById('items');
+    ul.innerHTML = ""; // alles löschen, da sonst doppelte einträge entstehen
+
+    for (let i = 0; i < cart.length; i++)
+    {
+        // um items wieder zu entfernen (- Button einbauen)
+
+        let removeButton = document.createElement('button');
+        removeButton.textContent = "-";
+        removeButton.addEventListener('click', function()
+        {
+            cart.splice(i, 1); // aus dem array an der pos i genau ein item entfernen
+            updateCart();
+            updatePrice();
+        })
+
+        let li = document.createElement('li');
+        li.textContent = "Artikel-ID: " + cart[i].id + ", " + cart[i].name + ", Preis: " + cart[i].price + "€\t";
+        li.appendChild(removeButton);
+        ul.appendChild(li);
+    }
+
+}
+function updatePrice ()
+{
+    let sum = 0;
+    for (let i = 0; i < cart.length; i++)
+    {
+        sum += cart[i].price;
+    }
+    let totalPrice = document.getElementById('total-price');
+    totalPrice.textContent = sum.toFixed(2); // Zahl mit 2 Nachkommastellen into String
+}
--- abalo/resources/views/articleView.blade.php
+++ abalo/resources/views/articleView.blade.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Artikel Übersicht</title>
+    <link rel="stylesheet" href="{{ asset('/css/cart.css') }}">
+
 </head>
 <body>
 <h1>Artikel Übersicht</h1>
 
+<div id="cart">
+    <h2>Warenkorb</h2>
+    <ul id="items"></ul> <!-- hier ul weil wir mit JS li´s einfügen werden -->
+    <p>Gesamtpreis: <span id="total-price">0.00</span> &euro;</p>
+</div>
+
 <form method="GET" action="/articles">
-    <label for="search">Suchwort: </label>
+    <label for="search">Suchwort</label>
     <input type="text" id="search" name="search" value=<?php echo $_GET['search'] ?? '' ?>>
     <input type="submit" value="suchen">
 </form>
@@ -24,25 +32,35 @@
         <th>Verkäufer-ID</th>
         <th>Erstellungsdatum</th>
         <th>Bild</th>
+        <th>Aktion</th>
     </tr>
     </thead>
     <tbody>
     @if(isset($data))
-    @foreach($data as $article)
-        <tr>
-            <td>{{ $article->id }}</td>
-            <td>{{ $article->ab_name }}</td>
-            <td>{{ $article->ab_price / 100 }}&euro;</td>
-            <td>{{ $article->ab_description }}</td>
-            <td>{{ $article->ab_creator_id }}</td>
-            <td>{{ $article->ab_createdate }}</td>
-            <td><img src="{{ asset($article->ab_image) }}" alt="Bild" width="150" height="75"></td>
-        </tr>
-    @endforeach
+        @foreach($data as $article)
+            <tr>
+                <td>{{ $article->id }}</td>
+                <td>{{ $article->ab_name }}</td>
+                <td>{{ $article->ab_price / 100}}&euro;</td>
+                <td>{{ $article->ab_description }}</td>
+                <td>{{ $article->ab_creator_id }}</td>
+                <td>{{ $article->ab_createdate }}</td>
+                <td><img src="{{ $article->ab_image }}" alt="Bild" width="150" height="75" ></td>
+                <td>
+                    <button class="add-to-cart"
+                            data-id="{{ $article->id }}"
+                            data-name="{{ $article->ab_name }}"
+                            data-price="{{ $article->ab_price / 100 }}">
+                        +
+                    </button> <!-- die values werden in die HTML Attribute (z.b. data-id = article-id) gespeichert -> mit JS einfach auslesen -->
+                </td>
+            </tr>
+        @endforeach
     @endif
     </tbody>
 
 </table>
 
+<script src="{{ asset('/js/cart.js') }}"></script>
 </body>
 </html>
