109dea3656f53346827b547bc96b6dc67c8f7d5b
Bobby <Bobbyly04@gmail.com>
2025-04-25 19:42:33 +0200

    fixed task8: added "Nicht OK" button so the user can deny cookies

--- abalo/public/js/cookiecheck.js
+++ abalo/public/js/cookiecheck.js
@@ -1,16 +1,44 @@
 "use strict";
 
+/*
 function getCookie(cname)
 {
     let match = document.cookie.match(new RegExp(cname + "=([^;]+)"));
     return match ? match[1] : null;
 }
 
+
 function setCookie(cname, cvalue, exdays)
 {
     let d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toDateString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+ */
+
+// Methoden von W3Schools.com
+function getCookie(cname) { // Take the cookiename as parameter (cname)
+    let name = cname + "="; // Create a variable (name) with the text to search for (cname + "=")
+    let decodedCookie = decodeURIComponent(document.cookie);    // Decode the cookie string, to handle cookies with special characters, e.g. '$'
+    let ca = decodedCookie.split(';');  // Split document.cookie on semicolons into an array called ca (ca = decodedCookie.split(';')).
+    for (let i = 0; i < ca.length; i++) {   // Loop through the ca array, and read out each value c = ca[i]
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {   // remove leading white-spaces
+            c = c.substring(1); // returns a string that starts at index 1 (skipped the white-space)
+        }
+        if (c.indexOf(name) === 0) {    // if the cookie is found, return the value of the cookie
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";  // if the cookie is not found, return ""
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -30,14 +58,19 @@ if (!getCookie("acceptCookies"))
         z-index: 9999;
     `;
     banner.innerHTML = `
-        Diese Website verwendet Cookies. <button id="accept-cookies" style="margin-left: 10px;">OK</button>
+        Diese Website verwendet Cookies. Möchten Sie diese akzeptieren?
+        <button id="accept-cookies" style="margin-left: 10px;">OK</button>
+        <button id="deny-cookies">Nicht OK</button>
     `;
+
 
     document.body.appendChild(banner);
 
     document.getElementById("accept-cookies").addEventListener("click", function () {
             setCookie("acceptCookies", "true", 365);
             banner.remove();
-        }
-    )
+        });
+    document.getElementById("deny-cookies").addEventListener("click", function () {
+        banner.remove();
+        });
 }
