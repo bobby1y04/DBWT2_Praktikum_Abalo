702c215668c491a8f3469f7d85ae69e44392603b
Bobby <Bobbyly04@gmail.com>
2025-06-10 09:33:48 +0200

    P5: 10_06 1

--- abalo/resources/js/M5/Aufgabe234/sitebody.vue
+++ abalo/resources/js/M5/Aufgabe234/sitebody.vue
@@ -1,5 +1,10 @@
 <script>
     export default {
+        data() {
+          return {
+              home_clicked: false
+          }
+        },
         methods: {
 
             jumpToLink: function(id) {
@@ -20,8 +25,9 @@
 
 <template>
     <main>
+        <div v-if="home_clicked === false">
     <ul>
-        <li id="Home" @click="jumpToLink('Home')">Home</li>
+        <li id="Home" @click="this.$data.home_clicked = true">Home</li>
         <li>Kategorien</li>
         <li id="Verkaufen" @click="jumpToLink('Verkaufen')">Verkaufen</li>
         <li> Unternehmen
@@ -31,6 +37,10 @@
             </ul>
         </li>
     </ul>
+        </div>
+        <div v-else-if="home_clicked === true">
+            <button @click="home_clicked = false">zurück</button>
+        </div>
     </main>
 </template>
 
---
+++ abalo/resources/js/M5/Aufgabe5/artikelsuche.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    data() {
+        let articles
+    }
+
+}
+
+</script>
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+</style>
